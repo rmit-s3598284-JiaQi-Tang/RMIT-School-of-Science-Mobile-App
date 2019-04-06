c0e47fd3a0d2bcfa122fee70b3282d94351fcde6
JiaQi Tang <s3598284@student.rmit.edu.au>
2019-04-06 23:57:20 +1100

    optimising ui

--- SchoolOfScience/SchoolOfScience/ContentViewController.swift
+++ SchoolOfScience/SchoolOfScience/ContentViewController.swift
@@ -7,31 +7,15 @@
 //
 
 import UIKit
-import Firebase
 class ContentViewController: UIViewController {
 
-    @IBOutlet weak var nameLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.hideKeyboardWhenTappedAround() 
-        let firebaseAuth = Auth.auth()
-        guard let user = firebaseAuth.currentUser else {return}
-        guard let name = user.displayName else {return}
-        nameLabel.text = name
+
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-    @IBAction func logoTapped(_ sender: Any) {
+    @IBAction func backButtonTapped(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
 }
