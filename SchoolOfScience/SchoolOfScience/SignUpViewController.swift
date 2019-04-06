fdc0c10a67b0dbebc56f87a18693bb688c977bf0
JiaQi Tang <s3598284@student.rmit.edu.au>
2019-04-06 16:08:36 +1100

    sign up function finished

--- SchoolOfScience/SchoolOfScience/SignUpViewController.swift
+++ SchoolOfScience/SchoolOfScience/SignUpViewController.swift
@@ -7,14 +7,16 @@
 //
 
 import UIKit
-
+import FirebaseAuth
+import FirebaseUI
+import Firebase
 class SignUpViewController: UIViewController {
 
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var confirmPasswordTextField: UITextField!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,7 +24,55 @@ class SignUpViewController: UIViewController {
     }
 
     @IBAction func signUpButtonTapped(_ sender: Any) {
-        
+        guard let email = emailTextField.text else {return}
+        guard let name = nameTextField.text else {return}
+        guard let password = passwordTextField.text else {return}
+        guard let confirmPassword = confirmPasswordTextField.text else {return}
+
+        if (password != confirmPassword) {
+
+            let alert = UIAlertController(title: "Password does not match !", message: "", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: {Void in})
+            alert.addAction(okAction)
+            alert.setValue(NSAttributedString(string: alert.title!, attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 17, weight: UIFont.Weight.medium), NSAttributedString.Key.foregroundColor : UIColor.red]), forKey: "attributedTitle")
+            self.present(alert, animated: true, completion: nil)
+            return
+
+        } else {
+
+            Auth.auth().createUser(withEmail: email, password: password, completion: {(resulet, error) in
+
+                if let error = error {
+                    let alert = UIAlertController(title: error.localizedDescription, message: "", preferredStyle: .alert)
+                    let okAction = UIAlertAction(title: "OK", style: .default, handler: {Void in})
+                    alert.addAction(okAction)
+                    alert.setValue(NSAttributedString(string: alert.title!, attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 17, weight: UIFont.Weight.medium), NSAttributedString.Key.foregroundColor : UIColor.red]), forKey: "attributedTitle")
+                    self.present(alert, animated: true, completion: nil)
+
+                } else {
+
+                    if let user = resulet?.user {
+                        let changeRequest = user.createProfileChangeRequest()
+                        changeRequest.displayName = name
+                        changeRequest.commitChanges(completion: { error in
+                            if let error = error {
+                                let alert = UIAlertController(title: error.localizedDescription, message: "", preferredStyle: .alert)
+                                let okAction = UIAlertAction(title: "OK", style: .default, handler: {Void in})
+                                alert.addAction(okAction)
+                                alert.setValue(NSAttributedString(string: alert.title!, attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 17, weight: UIFont.Weight.medium), NSAttributedString.Key.foregroundColor : UIColor.red]), forKey: "attributedTitle")
+                                self.present(alert, animated: true, completion: nil)
+                                return
+                            }
+                        })
+                    }
+                    let alert = UIAlertController(title: "User created successfully !", message: "", preferredStyle: .alert)
+                    let okAction = UIAlertAction(title: "OK", style: .default, handler: {Void in self.dismiss(animated: true, completion: nil)})
+                    alert.addAction(okAction)
+                    self.present(alert, animated: true, completion: nil)
+                }
+            })
+
+        }
     }
 
     @IBAction func cancelButtonTapped(_ sender: Any) {
